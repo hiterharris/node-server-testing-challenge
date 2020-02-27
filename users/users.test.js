c2c704d4285bf6f8e8ac5e868b8f023d2e161678
Hiter Harris <hiterharris@comcast.net>
2020-02-27 18:51:26 -0500

    added tests to users

---
+++ users/users.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const server = require('../api/server');
+
+describe('users router', function() {
+    it('should run the tests', function() {
+        expect(true).toBe(true);
+    });
+
+    describe('GET /', function() {
+        it('should return 200 OK', function() {
+            return request(server).get('/api/users').then(res => {
+                expect(res.status).toBe(200);
+            });
+        });
+
+        it('should return cohorts as the router', function() {
+            return request(server).get('/api/users').then(res => {
+                expect(res.body.router).toBe('users');
+            });
+        });
+
+        it('should return JSON formatted body', function() {
+            return request(server).get('/api/users').then(res => {
+                expect(res.type).toMatch(/json/);
+            });
+        });
+    });
+});
